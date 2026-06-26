6fb92a1eb6d17fa4dd1a46515c3a3f281a32eacf
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-26 22:54:31 +0530

    add category filter buttons

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ search.addEventListener("keyup", () => {
     });
 
 });
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+
+filterButtons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        filterButtons.forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        button.classList.add("active");
+
+        const category = button.dataset.category;
+
+        cards.forEach((card) => {
+
+            if (
+                category === "all" ||
+                card.classList.contains(category)
+            ) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
